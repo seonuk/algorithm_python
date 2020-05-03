1f2d2d127c34ec5f8c126cc21ca8596972165a6a
seonuk <sunwook317@gmail.com>
2020-05-03 16:59:17 +0900

    counting sort

---
+++ sort/counting_sort.py
@@ -0,0 +1,19 @@
+def counting_sort(numbers, scope):
+    count = [0] * (scope + 1)
+    sorted_number = []
+
+    for i in numbers:
+        count[i] += 1
+
+    for i, v in enumerate(count):
+        for _ in range(v):
+            sorted_number.append(i)
+
+    return sorted_number
+
+
+
+if __name__ == "__main__":
+    numbers = [2,2,2,1,4,3,3,3,5,6,7,8,8,8,8,10,9,6,7,7,6]
+    print(counting_sort(numbers,10))
+
