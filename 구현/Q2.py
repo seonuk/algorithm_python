0db377d4b3424d61baa4e145a1a634e31fee7700
seonuk <sunwook317@hanmail.net>
2020-11-24 17:55:42 +0900

    구현 Q2

---
+++ 구현/Q2.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+count = 0
+
+for h in range(N + 1):
+    for m in range(60):
+        for s in range(60):
+            if '3' in str(h) or '3' in str(m) or '3' in str(s):
+                count = count + 1
+
+print(count)
